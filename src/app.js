12a4f6ce642add0fdd9d5e71af89a00913420dda
institutosanluis <insitutosanluis@gmail.com>
2025-02-16 02:14:38 -0600

    Mod 16 02 2025

--- src/app.js
+++ src/app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+
 const comercioRoutes = require('./routes/comercioRoutes');
 
 const app = express();
+app.use(express.json({ limit: '990mb' }));
+app.use(express.urlencoded({ limit: '990mb', extended: true, parameterLimit: 900000 }));
+
 app.use(cors());
 app.use(express.json());
 
